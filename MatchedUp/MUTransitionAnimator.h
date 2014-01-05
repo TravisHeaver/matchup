7fba66afc3f35eb15837461d58fabe54dc839385
Travis Heaver <travisheaver@Traviss-MacBook-Pro.local>
2014-01-05 16:30:30 -0600

    functionality all added, can be cleaned up. I seem to have a bug when a second user is added

---
+++ MatchedUp/MUTransitionAnimator.h
@@ -0,0 +1,13 @@
+//
+//  MUTransitionAnimator.h
+//  MatchedUp
+//
+//  Created by Travis Heaver on 1/5/14.
+//  Copyright (c) 2014 Travis Heaver. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MUTransitionAnimator : NSObject
+
+@end
---
+++ MatchedUp/MUTransitionAnimator.m
@@ -0,0 +1,13 @@
+//
+//  MUTransitionAnimator.m
+//  MatchedUp
+//
+//  Created by Travis Heaver on 1/5/14.
+//  Copyright (c) 2014 Travis Heaver. All rights reserved.
+//
+
+#import "MUTransitionAnimator.h"
+
+@implementation MUTransitionAnimator
+
+@end
